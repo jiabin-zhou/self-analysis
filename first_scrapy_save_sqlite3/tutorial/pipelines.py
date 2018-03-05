0f2f505db1547425493deecc42ca900b9062a40c
jiabin-zhou <774124578@qq.com>
2018-03-05 16:26:27 +0800

    scrapy update

--- first_scrapy_save_sqlite3/tutorial/pipelines.py
+++ first_scrapy_save_sqlite3/tutorial/pipelines.py
@@ -4,12 +4,18 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import sqlite3
+# import sqlite3
+import pymysql
 
 class TutorialPipeline(object):
     def open_spider(self,spider):
         #链接数据库
-        self.conn=sqlite3.connect('data.sqlite')
+        self.conn=pymysql.connect(host='localhost',
+                                  port=3306,
+                                  db='mysql_test',
+                                  user='root',
+                                  password='root',
+                                  charset='utf8')
         #创建游标
         self.cursor=self.conn.cursor()
     def close_spider(self,spider):
@@ -17,9 +23,27 @@ class TutorialPipeline(object):
         self.conn.close()
     def process_item(self, item, spider):
         #向数据库data表中新增数据
-        insert="insert into data (appid,date, count, rate) VALUES ('{}','{}','{}','{}')".format(item['appid'],item['date'],item['count'],item['rate'])
+        insert="insert into test1 (appid,date,count,rate) VALUES ('{}','{}','{}','{}')".format(item['appid'],item['date'],item['count'],item['rate'])
         print(insert)
         cursor=self.cursor.execute(insert)
         #提交事务！
         self.conn.commit()
         return item
+
+# class TutorialPipeline(object):
+#     def open_spider(self,spider):
+#         #链接数据库
+#         self.conn=sqlite3.connect('data.sqlite')
+#         #创建游标
+#         self.cursor=self.conn.cursor()
+#     def close_spider(self,spider):
+#         #关闭数据库链接
+#         self.conn.close()
+#     def process_item(self, item, spider):
+#         #向数据库data表中新增数据
+#         insert="insert into data (appid,date, count, rate) VALUES ('{}','{}','{}','{}')".format(item['appid'],item['date'],item['count'],item['rate'])
+#         print(insert)
+#         cursor=self.cursor.execute(insert)
+#         #提交事务！
+#         self.conn.commit()
+#         return item
